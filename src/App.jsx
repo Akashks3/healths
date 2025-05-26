3572f3090c638093421be3b73c3a2acc7310af57
akash <155128336+Akashks3@users.noreply.github.com>
2025-05-26 20:56:21 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Sidebar from './Sidebar';
 import './App.css'; 
-import DashboardMainContent from './DashboardMainContent';
+import DashboardMainContent from './DashboardMainContent.jsx';
 const App = () => {
   return (
     <div className="app-container">
